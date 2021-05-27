62a774b6f2f6ac7600419c9c1eeb1f98e515c28e
MaresR0bert <robi_mares@yahoo.com>
2021-05-27 19:19:40 +0300

    seminar12

--- seminar11/seminar11template/src/ro/ase/acs/main/Main.java
+++ seminar11/seminar11template/src/ro/ase/acs/main/Main.java
@@ -6,10 +6,21 @@ import ro.ase.acs.template.RomanianCusineLunch;
 
 public class Main {
     public static void main(String[] args) {
-        LunchTemplate lunchTemplate = new FineDiningLunch();
-        lunchTemplate.takeLunch();
-        System.out.println(System.lineSeparator());
-        lunchTemplate = new RomanianCusineLunch();
-        lunchTemplate.takeLunch();
+//        LunchTemplate lunchTemplate = new FineDiningLunch();
+//        lunchTemplate.takeLunch();
+//        System.out.println(System.lineSeparator());
+//        lunchTemplate = new RomanianCusineLunch();
+//        lunchTemplate.takeLunch();
+
+        String s = "something";
+        String t = "something";
+        System.out.println(Integer.toHexString(s.hashCode()));
+        System.out.println(Integer.toHexString(t.hashCode()));
+        System.out.println(System.identityHashCode(s));
+        System.out.println(System.identityHashCode(t));
+        System.out.println(System.identityHashCode("something"));
+        if(s == t){
+            System.out.println("Correct");
+        }
     }
 }
---
+++ seminar12/seminar12bicep/src/ro/ase/acs/exceptions/InsufficentFunds.java
@@ -0,0 +1,5 @@
+package ro.ase.acs.exceptions;
+
+public class InsufficentFunds extends Exception {
+
+}
